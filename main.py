abae9f1644dc481c850127d18dfec346aae2bcf0
DITENROBLOX <mzubairov@gmail.com>
2025-10-10 21:19:16 +0500

    Create main.py
    jghgdiun

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное",
+            "ТРЭШ": "ложная, фиговая информация",
+            "DITEN": "сАмОе ВрЕмЯ пОдПиСаТсЯ нА кАнАл: https://www.youtube.com/channel/UCmsA6EruUyu0qLWDEXGFxCQ/"
+            }
+print('я тотя❤️')
+
+for i in range(5)
+    word = input("Введите непонятное слово (большими буквами!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print('TAKOГO CЛOBA HET')
