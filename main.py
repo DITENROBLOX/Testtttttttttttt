3a86da6887a614f22a2aa3bf77acb3855c5d5bc6
DITENROBLOX <mzubairov@gmail.com>
2025-10-17 21:21:12 +0500

    main.py
    drthn

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ meme_dict = {
             "КРИНЖ": "Что-то очень странное или стыдное",
             "ЛОЛ": "Что-то очень смешное",
             "ТРЭШ": "ложная, фиговая информация",
-            "DITEN": "сАмОе ВрЕмЯ пОдПиСаТсЯ нА кАнАл: https://www.youtube.com/channel/UCmsA6EruUyu0qLWDEXGFxCQ/"
+            "DITEN": "сАмОе ВрЕмЯ пОдПиСаТсЯ нА кАнАл: https://www.youtube.com/channel/UCmsA6EruUyu0qLWDEXGFxCQ/",
+            "KODLAND": "Вы только что получили скидку 15%!",
+            "RIDEACTION": "Вы получили скидку 20% на все товары в магазине RideAction!!!"
             }
-print('я тотя❤️')
-
-for i in range(5)
-    word = input("Введите непонятное слово (большими буквами!): ")
-    if word in meme_dict.keys():
+word = input("Введите непонятное слово (большими буквами!): ")
+if word in meme_dict.keys():
         print(meme_dict[word])
-    else:
+else:
         print('TAKOГO CЛOBA HET')
